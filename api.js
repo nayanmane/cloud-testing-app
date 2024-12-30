42e5b2021134bd746c1f1f2c97099c2ff7006660
nayanmane <100409583+nayanmane@users.noreply.github.com>
2024-12-30 15:27:36 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,12 @@
+
+var express = require("express");
+require('dotenv').config()
+const path = require('path');
+var app = express();
+const Router = require("./Routes/IndexRouter");
+app.use(express.static(path.join(__dirname)));
+app.use("/api", Router);
+
+var port = process.env.PORT || 8090;
+app.listen(port);
+console.log("Order API is runnning at " + port);
